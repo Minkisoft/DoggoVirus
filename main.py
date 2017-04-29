c315a5267779574320d8b85b5ca0b78d5fb29c84
Minkisoft <james.betson@blueyonder.co.uk>
2017-04-29 12:22:37 +0100

    Added the virus software
    Doggo gets placed onto the screen in a window once

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from tkinter import *
+root = Tk()
+
+root.title("Doggo")
+canvas = Canvas(root, width=310, height=280)
+canvas.pack()
+
+img = PhotoImage(file="doggo.gif")
+canvas.create_image(30,30, anchor=NW, image=img)
+
+message = Label(root, text = "Doggo has infected your computer", font = "Helvetica 16 bold italic")
+message.pack()
+
+
+root.mainloop()
 
